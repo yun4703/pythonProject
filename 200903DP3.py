c6029f378b227771c9b5f5d2629921d85b55368f
yun4703 <yun4703@naver.com>
2020-09-03 16:46:44 +0900

    직접푼거

---
+++ 200903DP3.py
@@ -0,0 +1,12 @@
+N = int(input())
+d = [[0 for _ in range(10)] for _ in range(1001)]
+for i in range(10):
+    d[1][i] = 1
+
+
+for i in range(2, N+1):
+    for j in range(10):
+        for k in range(j+1):
+            d[i][j] += d[i-1][k]
+
+print(sum(d[N])%10007)
